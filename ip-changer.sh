537913ab4a3484d922f4189b90c25329927334a9
heisen273 <f0xkid@ukr.net>
2021-01-24 13:52:43 +0100

    Update ip-changer.sh

--- ip-changer.sh
+++ ip-changer.sh
@@ -1,221 +1,221 @@
-#!/bin/bash
+# #!/bin/bash
 
-REFRESH_TEMPLATE_INTERVAL=43200
-REFRESH_SERVER_LIST_INTERVAL=3600
+# REFRESH_TEMPLATE_INTERVAL=43200
+# REFRESH_SERVER_LIST_INTERVAL=3600
 
-HMA_LOG_FILE=/var/log/openvpn_hma.log
-HMA_CREDENTIALS_FILE=/tmp/hmalogin
+# HMA_LOG_FILE=/var/log/openvpn_hma.log
+# HMA_CREDENTIALS_FILE=/tmp/hmalogin
 
-if [ -z ${HMA_COUNTRIES+x} ]; then
-    # Default: european countries
-    HMA_COUNTRIES="at hr cz dk fr de hu ie lu nl no pl ro sk ch gb ua it es lt se gr mt"
-fi
+# if [ -z ${HMA_COUNTRIES+x} ]; then
+#     # Default: european countries
+#     HMA_COUNTRIES="at hr cz dk fr de hu ie lu nl no pl ro sk ch gb ua it es lt se gr mt"
+# fi
 
-cd "`dirname \"$0\"`"
-
-
-serverList=()
-count=0
-function refreshServerList() {
-    echo "Downloading list of servers..."
-    grep=""
-    for c in ${HMA_COUNTRIES}; do
-        if [ "$grep" != "" ]; then grep="$grep\|"; fi
-        grep="$grep|$c|"
-    done
-    curl https://s3.amazonaws.com/hma-zendesk/linux/servers-cli.txt > /tmp/hma-servers
-    #curl https://web.archive.org/web/20170407211400/https://securenetconnection.com/vpnconfig/servers-cli.php > /tmp/hma-servers
-    serverList=`cat /tmp/hma-servers`
-    count=`echo "$serverList" | wc -l`
-    # Explode
-    OIFS=$IFS; IFS=$'\n'; serverList=(${serverList}); IFS=${OIFS}
-    echo "List of servers are ready, found $count servers."
-}
-
-function refreshTemplate() {
-    echo "Downloading OpenVPN config template..."
-    curl -s "https://securenetconnection.com/vpnconfig/openvpn-template.ovpn" | \
-        grep -v "auth-user-pass"  > /tmp/hma-template_.conf
-
-    if [ $? -eq 0 ]; then
-        mv /tmp/hma-template_.conf /tmp/hma-template.conf
-        echo "proto udp" >> /tmp/hma-template.conf
-        echo "port 53" >> /tmp/hma-template.conf
-        echo "auth-user-pass ${HMA_CREDENTIALS_FILE}" >> /tmp/hma-template.conf
-        echo "auth-nocache" >> /tmp/hma-template.conf
-
-        echo "OpenVPN config template is downloaded."
-    else
-        echo "ERROR: template couldn't been downloaded!"
-    fi
-}
-
-function killVPN() {
-    local vpnNum=$1
-    # Kill existing OpenVPN if necessery
-    if [ "$vpnNum" != "" ]; then
-        # Remove credentials file
-        rm "$HMA_CREDENTIALS_FILE" &>/dev/null
-
-        pid=`cat /var/run/openvpn${vpnNum}.pid 2>/dev/null`
-        # Kill process
-        kill ${pid} &>/dev/null
-        # Wait for process exiting
-        retry=50; while kill -s 0 ${pid} &>/dev/null; do sleep 0.1; let retry=retry-1; [ ${retry} -le 0 ] && break; done
-        # Force kill if still running
-        kill -9 ${pid} &>/dev/null
-
-        rm "/var/run/openvpn${vpnNum}.pid" &>/dev/null
-        rm "${HMA_LOG_FILE}_${vpnNum}" &>/dev/null
-        touch "${HMA_LOG_FILE}_${vpnNum}" &>/dev/null
-    fi
-}
+# cd "`dirname \"$0\"`"
 
 
-defaultGw=`ip route list | grep default | awk '{print $3}'`
-lastIP1=""; lastIP2=""
-function startNewVPN() {
-    local pid
-    local serverNum=$1
+# serverList=()
+# count=0
+# function refreshServerList() {
+#     echo "Downloading list of servers..."
+#     grep=""
+#     for c in ${HMA_COUNTRIES}; do
+#         if [ "$grep" != "" ]; then grep="$grep\|"; fi
+#         grep="$grep|$c|"
+#     done
+#     curl https://s3.amazonaws.com/hma-zendesk/linux/servers-cli.txt > /tmp/hma-servers
+#     #curl https://web.archive.org/web/20170407211400/https://securenetconnection.com/vpnconfig/servers-cli.php > /tmp/hma-servers
+#     serverList=`cat /tmp/hma-servers`
+#     count=`echo "$serverList" | wc -l`
+#     # Explode
+#     OIFS=$IFS; IFS=$'\n'; serverList=(${serverList}); IFS=${OIFS}
+#     echo "List of servers are ready, found $count servers."
+# }
 
-    # Create credentials file
-    echo -e "$HMA_USER\n$HMA_PASS" >${HMA_CREDENTIALS_FILE}
-    chmod 0600 ${HMA_CREDENTIALS_FILE}
+# function refreshTemplate() {
+#     echo "Downloading OpenVPN config template..."
+#     curl -s "https://securenetconnection.com/vpnconfig/openvpn-template.ovpn" | \
+#         grep -v "auth-user-pass"  > /tmp/hma-template_.conf
 
-    # Select random server from matched servers
-    eval "newIp=\"${lastIP1}\""
-    server=""
-    random=0
-    while [ "$lastIP1" == "$newIp" ] || [ "$lastIP2" == "$newIp" ]; do
-        random=$(($RANDOM%$count))
-        server=${serverList[random]}
-        # Explode server data
-        OIFS=$IFS; IFS='|'; server=(${server}); IFS=${OIFS}
-        newIp="${server[0]}"
-    done
+#     if [ $? -eq 0 ]; then
+#         mv /tmp/hma-template_.conf /tmp/hma-template.conf
+#         echo "proto udp" >> /tmp/hma-template.conf
+#         echo "port 53" >> /tmp/hma-template.conf
+#         echo "auth-user-pass ${HMA_CREDENTIALS_FILE}" >> /tmp/hma-template.conf
+#         echo "auth-nocache" >> /tmp/hma-template.conf
 
-    echo "Selected server($serverNum, $random): ${server[1]}, ${server[0]}"
+#         echo "OpenVPN config template is downloaded."
+#     else
+#         echo "ERROR: template couldn't been downloaded!"
+#     fi
+# }
 
-    # Create config
-    conf="/tmp/hma${serverNum}.conf"
-    cp /tmp/hma-template.conf ${conf}
-    # Add a few config lines we'll need
-    echo "suppress-timestamps" >> ${conf}
-    echo "verb 0" >> ${conf}
-    echo "log-append ${HMA_LOG_FILE}_${serverNum}" >> ${conf}
-    echo "remote ${server[0]} 53" >> ${conf}
-    echo "script-security 3" >> ${conf}
-    echo "route-noexec" >> ${conf}
-    echo "up /opt/up.sh" >> ${conf}
-    echo "route-up /opt/route-up.sh" >> ${conf}
-    echo "down /opt/down.sh" >> ${conf}
-    echo "dev tun$serverNum" >> ${conf}
+# function killVPN() {
+#     local vpnNum=$1
+#     # Kill existing OpenVPN if necessery
+#     if [ "$vpnNum" != "" ]; then
+#         # Remove credentials file
+#         rm "$HMA_CREDENTIALS_FILE" &>/dev/null
 
-    # Add route to this host on eth0
-    route add -host ${server[0]} gw ${defaultGw} &>/dev/null
+#         pid=`cat /var/run/openvpn${vpnNum}.pid 2>/dev/null`
+#         # Kill process
+#         kill ${pid} &>/dev/null
+#         # Wait for process exiting
+#         retry=50; while kill -s 0 ${pid} &>/dev/null; do sleep 0.1; let retry=retry-1; [ ${retry} -le 0 ] && break; done
+#         # Force kill if still running
+#         kill -9 ${pid} &>/dev/null
 
-    echo "Starting OpenVPN($serverNum)..."
-    /usr/sbin/openvpn --daemon --writepid /var/run/openvpn${serverNum}.pid --config ${conf}
-
-    if [ $? -eq 0 ]; then
-        echo "OpenVPN($serverNum) has started."
-        eval "lastIP${serverNum}=\"${server[0]}\""
-    else
-        echo "ERROR: OpenVPN has not started." >&2
-    fi
-}
-
-if [ -z ${HMA_USER+x} ]; then
-    # Get username from input
-    read -p "- HMA! Username: " HMA_USER < /dev/tty
-fi
-
-if [ -z ${HMA_PASS+x} ]; then
-    # Get password from input
-    read -s -p "- HMA! Password: " HMA_PASS < /dev/tty
-fi
+#         rm "/var/run/openvpn${vpnNum}.pid" &>/dev/null
+#         rm "${HMA_LOG_FILE}_${vpnNum}" &>/dev/null
+#         touch "${HMA_LOG_FILE}_${vpnNum}" &>/dev/null
+#     fi
+# }
 
 
-command="$1"
+# defaultGw=`ip route list | grep default | awk '{print $3}'`
+# lastIP1=""; lastIP2=""
+# function startNewVPN() {
+#     local pid
+#     local serverNum=$1
 
-# Kill existing vpn
-if [ "$command" == "kill" ]; then
-    vpnNum="$2"
-    if [ "$vpnNum" == "all" ]; then
-        killVPN 1
-        killVPN 2
-    else
-        killVPN "$vpnNum"
-    fi
+#     # Create credentials file
+#     echo -e "$HMA_USER\n$HMA_PASS" >${HMA_CREDENTIALS_FILE}
+#     chmod 0600 ${HMA_CREDENTIALS_FILE}
 
-    exit 0
-fi
+#     # Select random server from matched servers
+#     eval "newIp=\"${lastIP1}\""
+#     server=""
+#     random=0
+#     while [ "$lastIP1" == "$newIp" ] || [ "$lastIP2" == "$newIp" ]; do
+#         random=$(($RANDOM%$count))
+#         server=${serverList[random]}
+#         # Explode server data
+#         OIFS=$IFS; IFS='|'; server=(${server}); IFS=${OIFS}
+#         newIp="${server[0]}"
+#     done
 
-# Restart vpn by sending signal to the init process
-if [ "$command" == "change" ]; then
-    eval "kill -SIGUSR$2 `pgrep -f \"$0\" -o`"
-    exit 0
-fi
+#     echo "Selected server($serverNum, $random): ${server[1]}, ${server[0]}"
 
-if [ "$command" != "init" ]; then
-    echo "Unknown command: $command !" >&2
-    exit 1
-fi
+#     # Create config
+#     conf="/tmp/hma${serverNum}.conf"
+#     cp /tmp/hma-template.conf ${conf}
+#     # Add a few config lines we'll need
+#     echo "suppress-timestamps" >> ${conf}
+#     echo "verb 0" >> ${conf}
+#     echo "log-append ${HMA_LOG_FILE}_${serverNum}" >> ${conf}
+#     echo "remote ${server[0]} 53" >> ${conf}
+#     echo "script-security 3" >> ${conf}
+#     echo "route-noexec" >> ${conf}
+#     echo "up /opt/up.sh" >> ${conf}
+#     echo "route-up /opt/route-up.sh" >> ${conf}
+#     echo "down /opt/down.sh" >> ${conf}
+#     echo "dev tun$serverNum" >> ${conf}
+
+#     # Add route to this host on eth0
+#     route add -host ${server[0]} gw ${defaultGw} &>/dev/null
+
+#     echo "Starting OpenVPN($serverNum)..."
+#     /usr/sbin/openvpn --daemon --writepid /var/run/openvpn${serverNum}.pid --config ${conf}
+
+#     if [ $? -eq 0 ]; then
+#         echo "OpenVPN($serverNum) has started."
+#         eval "lastIP${serverNum}=\"${server[0]}\""
+#     else
+#         echo "ERROR: OpenVPN has not started." >&2
+#     fi
+# }
+
+# if [ -z ${HMA_USER+x} ]; then
+#     # Get username from input
+#     read -p "- HMA! Username: " HMA_USER < /dev/tty
+# fi
+
+# if [ -z ${HMA_PASS+x} ]; then
+#     # Get password from input
+#     read -s -p "- HMA! Password: " HMA_PASS < /dev/tty
+# fi
 
 
-# Signal handling
+# command="$1"
 
-function _trap() {
-    if [ -f /var/run/openvpn1.pid ]; then killVPN 1; fi
-    if [ -f /var/run/openvpn2.pid ]; then killVPN 2; fi
-    # To be sure
-    killall -9 openvpn &>/dev/null
-    exit 143
-}
-trap _trap SIGINT SIGTERM
+# # Kill existing vpn
+# if [ "$command" == "kill" ]; then
+#     vpnNum="$2"
+#     if [ "$vpnNum" == "all" ]; then
+#         killVPN 1
+#         killVPN 2
+#     else
+#         killVPN "$vpnNum"
+#     fi
 
-### VPN change by signal
+#     exit 0
+# fi
 
-function _changeVPN1() {
-    killVPN 1
-    startNewVPN 1
-}
+# # Restart vpn by sending signal to the init process
+# if [ "$command" == "change" ]; then
+#     eval "kill -SIGUSR$2 `pgrep -f \"$0\" -o`"
+#     exit 0
+# fi
 
-function _changeVPN2() {
-    killVPN 2
-    startNewVPN 2
-}
-trap _changeVPN1 SIGUSR1
-trap _changeVPN2 SIGUSR2
+# if [ "$command" != "init" ]; then
+#     echo "Unknown command: $command !" >&2
+#     exit 1
+# fi
 
-### Main cycle ###
 
-# Start initial VPN's
-killall -9 openvpn &>/dev/null
+# # Signal handling
 
-# Start initial VPNs
-refreshServerList
-refreshTemplate
-startNewVPN 1
-startNewVPN 2
+# function _trap() {
+#     if [ -f /var/run/openvpn1.pid ]; then killVPN 1; fi
+#     if [ -f /var/run/openvpn2.pid ]; then killVPN 2; fi
+#     # To be sure
+#     killall -9 openvpn &>/dev/null
+#     exit 143
+# }
+# trap _trap SIGINT SIGTERM
 
-refreshServerListCounter=0
-refreshTemplateCounter=0
+# ### VPN change by signal
 
-# Refresh template and server list
-while true; do
-    if [ ${refreshServerListCounter} -ge ${REFRESH_SERVER_LIST_INTERVAL} ]; then
-        refreshServerList
-        refreshServerListCounter=0
-    fi
+# function _changeVPN1() {
+#     killVPN 1
+#     startNewVPN 1
+# }
 
-    if [ ${refreshTemplateCounter} -ge ${REFRESH_TEMPLATE_INTERVAL} ]; then
-        refreshTemplate
-        refreshTemplateCounter=0
-    fi
+# function _changeVPN2() {
+#     killVPN 2
+#     startNewVPN 2
+# }
+# trap _changeVPN1 SIGUSR1
+# trap _changeVPN2 SIGUSR2
 
-    let "refreshServerListCounter=refreshServerListCounter+1"
-    let "refreshTemplateCounter=refreshTemplateCounter+1"
+# ### Main cycle ###
 
-    sleep 1
-done
+# # Start initial VPN's
+# killall -9 openvpn &>/dev/null
+
+# # Start initial VPNs
+# refreshServerList
+# refreshTemplate
+# startNewVPN 1
+# startNewVPN 2
+
+# refreshServerListCounter=0
+# refreshTemplateCounter=0
+
+# # Refresh template and server list
+# while true; do
+#     if [ ${refreshServerListCounter} -ge ${REFRESH_SERVER_LIST_INTERVAL} ]; then
+#         refreshServerList
+#         refreshServerListCounter=0
+#     fi
+
+#     if [ ${refreshTemplateCounter} -ge ${REFRESH_TEMPLATE_INTERVAL} ]; then
+#         refreshTemplate
+#         refreshTemplateCounter=0
+#     fi
+
+#     let "refreshServerListCounter=refreshServerListCounter+1"
+#     let "refreshTemplateCounter=refreshTemplateCounter+1"
+
+#     sleep 1
+# done
